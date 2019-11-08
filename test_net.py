2f4051ab5cf966fefb97c3eaeb3b6f5b53340e09
francois.masson <francois.masson@leddartech.com>
2019-11-08 12:09:06 -0500

    function test_net

--- test_net.py
+++ test_net.py
@@ -18,14 +18,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-#from test_helper import *
+from test_helper import *
 
 from scipy.spatial.distance import cdist
 from matplotlib import gridspec
-
 import textwrap as tw
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+def show(image_1,image_2,similarity, dissimilarity, euc, my_logits):
+    fig = plt.figure()
+    plt.title(('Similarity: %f, Dissimilarity: %f\nEuclidean Dist: %f, Logits: %f' % (similarity, dissimilarity, euc, my_logits)), loc='center')
+    if my_logits > 0.0:
+        textstr = 'Similar'
+        props = dict(boxstyle='round', facecolor='green', alpha=0.5)
+        fig_txt = tw.fill(tw.dedent(textstr), width=80)
+        plt.figtext(0.51, 0.05, fig_txt, horizontalalignment='center',
+                    fontsize=12, multialignment='center',
+                    bbox=dict(boxstyle="round", facecolor='green',
+                                ec="0.5", pad=0.5, alpha=1), fontweight='bold')
+    else:
+        textstr = 'Dissimilar'
+        props = dict(boxstyle='round', facecolor='red', alpha=0.5)
+        fig_txt = tw.fill(tw.dedent(textstr), width=80)
+        plt.figtext(0.51, 0.05, fig_txt, horizontalalignment='center',
+                    fontsize=12, multialignment='center',
+                    bbox=dict(boxstyle="round", facecolor='red',
+                                ec="0.5", pad=0.5, alpha=1), fontweight='bold')
+
+
+
+    plt.axis('off')
+    ax1 = fig.add_subplot(1, 2, 1)
+    l_im = np.array(img1)[0]
+    ax1.imshow(l_im)
+    ax1.axis('off')
+    ax2 = fig.add_subplot(1, 2, 2)
+    r_im = np.array(img2)[0]
+    ax2.imshow(r_im)
+    ax2.axis('off')
+
+
+    plt.show()
 
 def main():
 
@@ -60,74 +92,42 @@ def main():
     logits, model_left, model_right = inference(left_input_im, right_input_im)
 
     # # total_loss = tf.losses.get_total_loss()
-    # global_step = tf.Variable(0, trainable=False)
-    # global_init = tf.variables_initializer(tf.global_variables())
+    global_step = tf.Variable(0, trainable=False)
+    global_init = tf.variables_initializer(tf.global_variables())
 
-    # saver = tf.train.Saver()
-    # with tf.Session() as sess:
-    #     sess.run(global_init)
-    #     ckpt = tf.train.get_checkpoint_state("model")
-    #     saver.restore(sess, "model_siamese/model.ckpt")
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(global_init)
+        ckpt = tf.train.get_checkpoint_state("model") #Validate .... what to feed ...
+        saver.restore(sess, "model_siamese/model.ckpt") # Validate and provide in the readme...
 
-    #     my_logits, model_lf, model_rg = sess.run([logits, model_left, model_right], \
-    #                                              feed_dict={left_input_im: img, right_input_im: img2})
+        my_logits, model_lf, model_rg = sess.run([logits, model_left, model_right], \
+                                                 feed_dict={left_input_im: img, right_input_im: img2})
 
-    #     print(my_logits)
-    #     print(np.shape(model_lf))
-    #     print(np.shape(model_rg))
+        print(my_logits)
+        print(np.shape(model_lf))
+        print(np.shape(model_rg))
 
-    #     lft = np.array(model_lf[0])
-    #     rgt = np.array(model_rg[0])
-    #     l = lft - rgt
+        lft = np.array(model_lf[0])
+        rgt = np.array(model_rg[0])
+        l = lft - rgt
 
-    #     distance = np.sqrt(np.sum((l) ** 2))
-    #     similarity = my_logits * np.square(distance)  # keep the similar label (1) close to each other
-    #     dissimilarity = (1 - np.array(my_logits[0])) * np.square(np.max((0.5 - distance),
-    #                                                                     0))  # give penalty to dissimilar label if the distance is bigger than margin
-    #     similarity_loss = np.mean(dissimilarity + similarity) / 2
-    #     print('distance : ', distance)
-    #     print('similarity : ', similarity)
-    #     print('dissimilarity : ', dissimilarity)
-    #     print('similarity_loss : ', similarity_loss)
+        distance = np.sqrt(np.sum((l) ** 2))
+        similarity = my_logits * np.square(distance)  # keep the similar label (1) close to each other
+        dissimilarity = (1 - np.array(my_logits[0])) * np.square(np.max((0.5 - distance),
+                                                                        0))  # give penalty to dissimilar label if the distance is bigger than margin
+        similarity_loss = np.mean(dissimilarity + similarity) / 2
+        print('distance : ', distance)
+        print('similarity : ', similarity)
+        print('dissimilarity : ', dissimilarity)
+        print('similarity_loss : ', similarity_loss)
 
-    #     dist = cdist(model_lf, model_rg, 'cosine')
-    #     print('Pairwise distance : ', dist)
-    #     euc = np.linalg.norm(model_lf - model_rg)
-    #     print('euc : ', euc)
+        dist = cdist(model_lf, model_rg, 'cosine')
+        print('Pairwise distance : ', dist)
+        euc = np.linalg.norm(model_lf - model_rg)
+        print('euc : ', euc)
 
-    #     fig = plt.figure()
-    #     plt.title(('Similarity: %f, Dissimilarity: %f\nEuclidean Dist: %f, Logits: %f' % (similarity, dissimilarity, euc, my_logits)), loc='center')
-    #     if my_logits > 0.0:
-    #         textstr = 'Similar'
-    #         props = dict(boxstyle='round', facecolor='green', alpha=0.5)
-    #         fig_txt = tw.fill(tw.dedent(textstr), width=80)
-    #         plt.figtext(0.51, 0.05, fig_txt, horizontalalignment='center',
-    #                     fontsize=12, multialignment='center',
-    #                     bbox=dict(boxstyle="round", facecolor='green',
-    #                               ec="0.5", pad=0.5, alpha=1), fontweight='bold')
-    #     else:
-    #         textstr = 'Dissimilar'
-    #         props = dict(boxstyle='round', facecolor='red', alpha=0.5)
-    #         fig_txt = tw.fill(tw.dedent(textstr), width=80)
-    #         plt.figtext(0.51, 0.05, fig_txt, horizontalalignment='center',
-    #                     fontsize=12, multialignment='center',
-    #                     bbox=dict(boxstyle="round", facecolor='red',
-    #                               ec="0.5", pad=0.5, alpha=1), fontweight='bold')
-
-
-
-    #     plt.axis('off')
-    #     ax1 = fig.add_subplot(1, 2, 1)
-    #     l_im = np.array(img)[0]
-    #     ax1.imshow(l_im)
-    #     ax1.axis('off')
-    #     ax2 = fig.add_subplot(1, 2, 2)
-    #     r_im = np.array(img2)[0]
-    #     ax2.imshow(r_im)
-    #     ax2.axis('off')
-
-
-    #     plt.show()
+        show(img1,img2,similarity, dissimilarity, euc, my_logits)
 
 if __name__ == '__main__':
     main()
